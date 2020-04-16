0b051a76b49cf5657813dfc7152d34b16ace5063
Cody Branson <codyboi7000@gmail.com>
2020-04-16 17:00:39 -0600

    new updates for day two of this project

--- src/App.js
+++ src/App.js
@@ -1,46 +1,58 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
-import Header from './components/Header';
-import Image from './components/Image';
-import Info from './components/Info'
 
-import axios from 'axios'
+//Reactstrap Impors
+import {Alert} from "reactstrap";
+import {Container, Row, Col} from "reactstrap";
 
-const url = 'https://api.nasa.gov/planetary/apod'
-const api_key = '4h3uoSzqgdqfMotMoqO6SyZqle2CyhDuUFfoOGCc'
+//Component Imports
+import Header from "./components/Header";
+import Image from "./components/Image";
+import Info from "./components/Info";
+import Footer from "./components/footer";
+import Columns from "./components/styles/Columns";
+import Navigation from "./components/styles/Navbar";
 
+//Style Imports
+import HeaderStyle from "./components/styles/HeaderStyling";
+import BodyStyling from "./components/styles/BodyStyling";
 
+import axios from "axios";
 
+const url = "https://api.nasa.gov/planetary/apod";
+const api_key = "4h3uoSzqgdqfMotMoqO6SyZqle2CyhDuUFfoOGCc";
 
 function App() {
-const [nasaData, setNasaData] = useState([])
-const [newPage, setNewPage] = useState([])
+  const [nasaData, setNasaData] = useState([]);
+  const [newPage, setNewPage] = useState([]);
 
-useEffect(() => {
-  axios.get(`${url}?api_key=${api_key}`)
-    .then(res =>{
-      console.log(res)
+  useEffect(() => {
+    axios.get(`${url}?api_key=${api_key}`).then(res => {
+      console.log(res);
       setNasaData(res.data);
-    })
-}, []);
-
-
-
-
-
-
+    });
+  }, []);
 
   return (
     <div className="App">
-      <Header title={nasaData.title} date={nasaData.date}/>
-      <Image url={nasaData.url}/>
+      <BodyStyling>
+        <Navigation />
 
-    <div className='Info'>
-        <Info explanation={nasaData.explanation}/>
+        <HeaderStyle>
+          <Header title={nasaData.title} date={nasaData.date}></Header>
+        </HeaderStyle>
+        <Image url={nasaData.url} />
 
-    </div>
+        <div className="Info">
+          <Info explanation={nasaData.explanation} />
+        </div>
+      </BodyStyling>
 
+      <div>
+        <Columns />
       </div>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/styles/HeaderStyling.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const HeaderStyle = styled.h1`
+  color: red;
+  margin: 8px;
+  padding: 12px;
+  background-color: white;
+
+
+  &:hover{
+    color:blue;
+    transition: all 1s ease-in-out;
+  }
+
+  
+`
+
+export default HeaderStyle
